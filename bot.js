8ffc56e8bd343eb286cc37bf24a00f2ad2baff1f
futurteam <68615153+futurteam@users.noreply.github.com>
2020-07-21 22:16:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Futur Team `,"http://twitch.tv/S-F")
+client.user.setGame(`Futur Team `,"http://twitch.tv/futur_team")
 client.user.setStatus("dnd")
 });
 
